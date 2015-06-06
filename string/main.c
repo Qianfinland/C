aca9e337e6676ea2118cb5ced4af3036f3296deb
Qianfinland <qianfinland@gmail.com>
2015-06-06 23:36:26 +0300

    count length before null

--- string/main.c
+++ string/main.c
@@ -15,10 +15,22 @@ int main(int argc, char** argv) {
     //array character declare, read and write
     char name[20];
     printf("Enter your name:\n");
-    //scanf("%s", name);
+    //scanf("%s", name); // not work with multiple words 
     gets(name);
-    //printf("The name you enter is %s\n", name);
-    puts( name);
+    printf("The name you enter is %s\n", name);
+    //puts(name);
+    
+    //count the length of the string before null
+    int count, len;
+    /*while(name[count] != '\0') {
+        count++;
+    }*/
+    
+    //valid for loop without statement
+    for(count=0; name[count] != '\0'; count++); 
+    
+    
+    printf("Length = %d\n",count);
     return (EXIT_SUCCESS);
 }
 
