f8e0b4f9b3f82c3d4d0c898b69be174d84f6c078
Qianfinland <qianfinland@gmail.com>
2015-06-15 21:50:36 +0300

    simple demo on call by value and call by reference

--- pointers/main.c
+++ pointers/main.c
@@ -13,27 +13,20 @@
  */
 
 int main() {
-    int i = 8;
-    printf("Value: %d\n", i);
-    printf("Address: %p\n", &i); //
-    printf("Value: %d\n", *(&i));
+    int n1, n2, n3;
+    n1 = n2 =n3 = 10;
+    int* pn2;
+    pn2 = &n2;
     
-    int* pi;
-    pi = &i; //pi is the address of type integer
-    i = 100;
-    printf("Address: %p\n", pi); //pi = &i
-    printf("Value: %d\n", *pi); //'pi = *(&i)
-    
-    // example of pointer arithmetic 
-    int a;
-    int* pa, pa1;
-    pa = &a;
-    pa1 = pa + 1; //pa1 and pa is 4 bytes diff
-    printf("%d, %d\n", pa, pa1);
-    
-    int da;
-    int* pda, pda1;
-    pda1 = pda + 1;
-    printf("%d, %d\n", pda, pda1);
+    n3++;
+    printf("%d\n", n3);
+    Foo(n1, pn2);
+    printf("%d %d\n", n1, n2); //10 and 11
     return 0;
+}
+
+
+void Foo(int n, int* pn) {
+    n++;
+    (*pn)++;
 }
