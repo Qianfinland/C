6a0154ed5ac0c1cc4bd98e5d0ab725fab93ae88d
Qianfinland <qianfinland@gmail.com>
2015-06-15 22:12:38 +0300

    demo by calling swappointer function by reference

--- pointers/main.c
+++ pointers/main.c
@@ -9,15 +9,15 @@
 #include <stdlib.h>
 
 /*
- * 
- */
-
+ 
 int main() {
     int n1, n2;
     n1 = 1;
     n2 = 2;
     swap(n1, n2);
+    //In swap method: 2 and 1
     printf("In main method: %d and %d\n", n1, n2); 
+    //In main method: 1 and 2
     return 0;
 }
 
@@ -28,4 +28,24 @@ void swap (int n, int m) {
     n = m;
     m = tem;
     printf("In swap method: %d and %d\n", n, m); 
+}
+ */
+int main() {
+    int n1, n2;
+    n1 = 1;
+    n2 = 2;
+    swappointer(&n1, &n2);
+    //In swappointer method: 2 and 1
+    printf("In main method: %d and %d\n", n1, n2); 
+    //In main method: 2 and 1
+    return 0;
+}
+
+
+void swappointer (int *pn, int *pm) {
+    int tem;
+    tem = *pn;
+    *pn = *pm;
+    *pm = tem;
+    printf("In swappointer method: %d and %d\n", *pn, *pm); 
 }
