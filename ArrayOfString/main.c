aa650d195d9d8ea8612ef295d58050c77a1ea105
Qianfinland <qianfinland@gmail.com>
2015-06-23 22:21:51 +0300

    concatenate all messages together

--- ArrayOfString/main.c
+++ ArrayOfString/main.c
@@ -5,17 +5,33 @@
  */
 
 #include <stdio.h>
+#include <string.h>//function strlen, strcpy, strcat
+#include <stdlib.h>// use malloc
 
 int main() {
-    char* messages[10] = {"One", "two", "three"};
+    char* messages[5] = {"One", "two", "three"};
+    char* allmessages;
     int i;
-    for(i = 0; i < 10; i++)
+    int total = 0;
+    for(i = 0; i < 5; i++)
     {
         printf("Enter string[%d]:\n", i);
         messages[i] = (char*)malloc(10);//without this have segment fault error
         scanf("%s", messages[i]);
+        //gets(messages[i]);
+        total += strlen(messages[i]);
     }
     
+    allmessages = (char*)malloc(total+1);
+    
+    strcpy(allmessages, messages[0]);//copy the first string in messages
+    
+    for(i=1; i<5; i++) //
+    {
+        strcat(allmessages, " "); //without this no space
+        strcat(allmessages, messages[i]);
+    }
+    printf("%s", allmessages);
     return 0;
 }
 
