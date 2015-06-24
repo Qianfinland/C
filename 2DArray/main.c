568ee77b426040a2f93eab67801ee1724f6fbdea
Qianfinland <qianfinland@gmail.com>
2015-06-24 22:10:01 +0300

    allow user to enter value of a declared 2D array

--- 2DArray/main.c
+++ 2DArray/main.c
@@ -6,14 +6,35 @@
 
 #include <stdio.h>
 
+void BuildMatrix(int ar[][4], int rows, int cols);
 int main() {
-    int arr[2][3] = {1,2,3,4,5,6};//={{1,2,3}, {4,5,6}};
+    /*initialize and declare 2D array
+     * int arr[2][3] = {1,2,3,4,5,6};//={{1,2,3}, {4,5,6}};
     int r, c;
     for(r=0; r<2; r++) {
         for(c=0;c<3; c++)
             printf("%d ", arr[r][c]);
         printf("\n");//print each row
+    }*/
+    
+    //Allow user to enter value for the 2D Array
+    int arr[3][4];//decalre 2D array
+    int r, c;
+    BuildMatrix(arr, 3, 4);//call method
+    for(r=0; r<3; r++) {
+        for(c=0;c<4; c++)
+            printf("%d ", arr[r][c]);
+        printf("\n");
     }
     return 0;
 }
 
+void BuildMatrix(int ar[][4], int rows, int cols) {
+    int i,j;
+    for(i=0;i<rows;i++) {
+        for(j=0;j<cols;j++) {
+            printf("Enter value %d row %d col:", i, j);
+            scanf("%d", &ar[i][j]);
+        }
+    }
+}
