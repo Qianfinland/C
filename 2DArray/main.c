2684b6727d6deff16778528a4c6c9a3c4a761d62
Qianfinland <qianfinland@gmail.com>
2015-06-24 22:18:01 +0300

    refactor code

--- 2DArray/main.c
+++ 2DArray/main.c
@@ -5,8 +5,9 @@
  */
 
 #include <stdio.h>
-
-void BuildMatrix(int ar[][4], int rows, int cols);
+#define Y 4
+void BuildMatrix(int ar[][Y], int rows, int cols);
+void PrintMatrix(int ar[][Y], int rows, int cols);
 int main() {
     /*initialize and declare 2D array
      * int arr[2][3] = {1,2,3,4,5,6};//={{1,2,3}, {4,5,6}};
@@ -20,21 +21,26 @@ int main() {
     //Allow user to enter value for the 2D Array
     int arr[3][4];//decalre 2D array
     int r, c;
-    BuildMatrix(arr, 3, 4);//call method
-    for(r=0; r<3; r++) {
-        for(c=0;c<4; c++)
-            printf("%d ", arr[r][c]);
-        printf("\n");
-    }
+    BuildMatrix(arr, 3, Y);//call method
+    PrintMatrix(arr, 3, Y);//call method to print
     return 0;
 }
 
-void BuildMatrix(int ar[][4], int rows, int cols) {
+void BuildMatrix(int ar[][Y], int rows, int cols) {
     int i,j;
     for(i=0;i<rows;i++) {
         for(j=0;j<cols;j++) {
             printf("Enter value %d row %d col:", i, j);
             scanf("%d", &ar[i][j]);
         }
+    }
+}
+
+void PrintMatrix(int ar[][Y], int rows, int cols) {
+    int r, c;
+    for (r = 0; r < rows; r++) {
+        for (c = 0; c < cols; c++)
+            printf("%d ", ar[r][c]);
+        printf("\n");
     }
 }
