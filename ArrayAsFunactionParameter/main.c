bc144714b1fe4cb96d9d84eccea47571e3ae0c8f
Qianfinland <qianfinland@gmail.com>
2015-06-26 22:18:59 +0300

    build your own array

--- ArrayAsFunactionParameter/main.c
+++ ArrayAsFunactionParameter/main.c
@@ -7,6 +7,7 @@
 
 void PrintArray(int arr[], int size);
 void IncreaseArray(int arr[], int size);
+void BuildArray(int arr[], int size);
 int main() {
     int a1[3] = {1, 2, 3};
     PrintArray(a1, 3);
@@ -14,6 +15,10 @@ int main() {
     IncreaseArray(a2, 5);
     //arr will change due modification in IncreaseArray function
     PrintArray(a2, 5); 
+    
+    int a3[2];
+    BuildArray(a3,2);
+    PrintArray(a3,2);
     return 0;
 }
 
@@ -30,4 +35,12 @@ void IncreaseArray(int arr[], int size) {
     int i;
     for(i = 0; i < size; i++)
         arr[i]++;
+}
+
+void BuildArray(int arr[], int size) {
+    int i;
+    for(i = 0; i < size; i++) {
+        printf("Enter array element [%d]:", i);
+        scanf("%d", &arr[i]);
+    }
 }
