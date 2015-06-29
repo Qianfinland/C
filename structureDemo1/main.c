fa60a23a841868e5c93b7c0088da4fdb5fbc37ba
Qianfinland <qianfinland@gmail.com>
2015-06-29 23:25:30 +0300

    strcut as a struct member

--- structureDemo1/main.c
+++ structureDemo1/main.c
@@ -4,12 +4,20 @@
  */
 
 #include <stdio.h>
+
+struct DetailedInfo
+{
+    char career[20];
+    int income;
+    
+};
  //declare a struct 
  struct Person
     {
      char name[30];
      int age;
      char address[50];
+     struct DetailedInfo details;
     };
 int main() {
     struct Person p1;//instantiate a structure
@@ -17,10 +25,13 @@ int main() {
     gets(p1.name);
     printf("Enter age: ");
     scanf("%d", &p1.age);
-    scanf("%c");
+    scanf("%c");//without this cannot enter address
     printf("Enter address: ");
     gets(p1.address);
     printf("%s with age %d lives in %s\n", p1.name, p1.age, p1.address);
+    printf("Enter career and income: ");
+    scanf("%s %d", &p1.details.career, &p1.details.income);
+    printf("%s works as %s and earns %d per month", p1.name, p1.details.career, p1.details.income);
     return 0;
 }
 
