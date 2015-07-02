10182a96deb5e9a16b001cc970cb8780303e91c8
Qianfinland <qianfinland@gmail.com>
2015-07-02 21:58:02 +0300

    same union instance but diffrent data types

--- union/main.c
+++ union/main.c
@@ -2,7 +2,61 @@
 
 #include <stdio.h>
 #include <string.h>
-typedef struct Demos
+
+typedef enum
+{
+    CHAR, SHORT, INTEGER, FLOAT, DOUBLE, STRING
+}Type;
+
+typedef union
+{
+    char Character;
+    short Short;
+    int Integer;
+    float Float;
+    double Double;
+    char String[30];
+} Value;
+
+typedef struct 
+{
+    Type type;
+    Value value;
+}Variant;
+
+void Foo(Variant v);
+int main( )
+{
+    Variant v1;
+    v1.type = INTEGER;
+    v1.value.Integer = 100;
+    Foo(v1);//100
+    
+    printf("Same instance but can be diff data type\n");
+    v1.type = STRING;
+    strcpy(v1.value.String, "v1 is String now");
+    //v1.value.String = "String now";// nooooot work
+    Foo(v1);//V1 is string now!
+}
+
+void Foo(Variant v)
+{
+    if(v.type == CHAR)
+        printf("%c\n", v.value.Character);
+    else if(v.type == SHORT)
+        printf("%d\n", v.value.Short);
+    else if(v.type == INTEGER)
+        printf("%d\n", v.value.Integer);
+    else if(v.type == FLOAT)
+        printf("%f\n", v.value.Float);
+    else if(v.type == DOUBLE)
+        printf("%lf\n", v.value.Double);
+    else if(v.type == STRING)
+        printf("%s\n", v.value.String);
+    else
+        printf("Error!\n");
+}
+/*typedef struct Demos
 {
     int n;
     double d;
@@ -33,5 +87,5 @@ int main() {
     printf("%ld %ld %ld\n", sizeof(u.n), sizeof(u.d), sizeof(u.str));
     printf("Size of the union is %ld\n", sizeof(u));
     return 0;
-}
+}*/
 
