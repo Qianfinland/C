3dbe489724970b84cada5f9b9fea56fbe0b9945c
Qianfinland <qianfinland@gmail.com>
2015-07-05 16:38:30 +0300

    while loop to save more students info

--- FileHandlingStruct/main.c
+++ FileHandlingStruct/main.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 struct Student
 {
@@ -14,21 +15,33 @@ struct Student
 } ;
 int main() {
     struct Student s1;
-    printf("Enter the RollNo: ");
-    scanf("%d", &s1.RollNo);
-    printf("Enter the name: ");
-    scanf("%s", s1.Name);
-    printf("Enter the points: ");
-    scanf("%lf", &s1.points);
-    
+    char ch;
     FILE *file = fopen("/home/qxzhou/NetBeansProjects/daydaylearning/FileHandlingStruct/student.txt", "w+");
     if(file == NULL)
     {
         printf("Error in saving student info\n");
         return 1;
     }
-    fprintf(file, "%d\t%s\t%lf\n", s1.RollNo, s1.Name, s1.points);
-    
+    while(1)
+    {
+        printf("Enter the RollNo: ");
+        scanf("%d", &s1.RollNo);
+        printf("Enter the name: ");
+        scanf("%s", s1.Name);
+        printf("Enter the points: ");
+        scanf("%lf", &s1.points);
+        fprintf(file, "%d\t%s\t%lf\n", s1.RollNo, s1.Name, s1.points);
+        
+        printf("Continue? (Y/N)");
+        do
+                ch = getchar();
+        while (isspace(ch));
+        //printf("ch is %c\n", ch);
+        if(ch == 'N' || ch == 'n')
+            break;
+    }
+   
+    fclose(file);
     return 0;
 }
 
