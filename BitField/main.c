09c9a40615d8cb7e107bd11e12a4439c4a2c4be6
Qianfinland <qianfinland@gmail.com>
2015-07-09 23:25:29 +0300

    usage of bit field in struct

--- BitField/main.c
+++ BitField/main.c
@@ -8,19 +8,29 @@
 
 struct 
 {
-    int mem1;
-    int mem2;
+    unsigned int mem1; //4 bits 1111, max value is 15
+    unsigned int mem2;
 } ns;
 
 struct 
 {
-    int mem1 : 5;
-    int mem2 : 4;
+    unsigned int mem1 : 3; //3 bits 111, max value is 7
+    unsigned int mem2 : 3;
 } sfb;
 
 int main(int argc, char** argv) {
     printf("Size of regular struct = %ld\n", sizeof(ns));//8
     printf("Size of struct with bit field = %ld\n", sizeof(sfb));//4
+    
+    ns.mem1 = 7;
+    sfb.mem1 = 7;
+    printf("value of mem1 in ns =%d\n", ns.mem1);//7
+    printf("value of mem1 in sfb =%d\n", sfb.mem1);//7
+    
+    ns.mem2 = 8;
+    sfb.mem2 = 8;
+    printf("value of mem2 in ns =%d\n", ns.mem2);//8
+    printf("value of mem2 in sfb =%d\n", sfb.mem2);//0
     return (EXIT_SUCCESS);
 }
 
