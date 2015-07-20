9f8bcd782a0643881bffbfb1ef0fa80b9528e342
Qianfinland <qianfinland@gmail.com>
2015-07-20 22:53:54 +0300

    header files demo

---
+++ HeaderFiles/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "sum.h"
+#include "sum.h"
+#include "sum.h"
+int main() {
+    int n1= 10, n2=50, result;
+    result = Add(n1, n2);
+    printf("Sum is %d\n", result);
+    return 0;
+}
+
---
+++ HeaderFiles/sum.h
@@ -0,0 +1,18 @@
+int Add(int a, int b);
+
+#ifndef SUM_H
+#define	SUM_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SUM_H */
+
