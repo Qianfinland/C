687f6e35f198e6db0fbaee96c20458cd51499581
Qianfinland <qianfinland@gmail.com>
2015-07-27 23:46:53 +0300

    day 3

--- Exercise2/main.c
+++ Exercise2/main.c
@@ -1,15 +1,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 void loop1();
 void sameAsLoop1();
 void loop2();
 void loop3();
-void loop();
-
+void instruct();
 int main() {
     
-    int n, i;
+    instruct();
+    char answer;
+    int a,b, n;
+    do
+    {
+        printf("Enter a number(1-5): ");
+        scanf("%d", &n);
+        printf("Enter two numbers: ");
+        scanf("%d %d", &a, &b);
+        if(n==1)
+        {add(a,b);}
+        else if(n==2)
+        {substract(a,b);}
+        else if(n==3)
+        {multiply(a,b);}
+        else if(n==4)
+        {divide(a,b);}
+        else if(n==5)
+        {module(a,b);}
+        else
+        {printf("Error\n");}
+        printf("Continue?(y/n)");
+        scanf("%s", &answer);
+    }while(answer=='y' || answer=='Y');
+ 
+    /*int n, i;
     int arr[n];
     printf("Enter number of positive numbers: ");
     scanf("%d", &n);
@@ -20,17 +45,39 @@ int main() {
         scanf("%d", &arr[i]);
         if (arr[i] < 0)
             break;
-    }
+    }*/
     //loop3();//sameAsLoop3();
     //loop2();
     //loop1();
     //sameAsLoop1();
     return 0;
 }
-void loop()
+
+void instruct()
 {
-    
+    printf("Calculator menu:\n");
+    printf("1. Add\n");
+    printf("2. Subtract\n");
+    printf("3, Multiply\n");
+    printf("4. Divide\n");
+    printf("5. Modulus\n");
 }
+int add(int a, int b)
+{printf("Result: %d\n", a+b);return a+b;}
+
+int substract(int a, int b)
+{printf("Result:%d\n", a-b);return a-b;}
+
+int multiply(int a, int b)
+{printf("Result:%d\n", a*b);return a*b;}
+
+int divide(int a, int b)
+{printf("Result:%d\n", a/b);return a/b;}
+
+int module(int a, int b)
+{printf("Result:%d\n", a%b);return a%b;}
+
+
 void sameAsLoop3()
 {
     int i, j;
