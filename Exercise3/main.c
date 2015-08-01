000f2936049a44ae9740651f7b1fb163347e7d50
Qianfinland <qianfinland@gmail.com>
2015-08-01 21:20:07 +0300

    pick up max and min in three integers

--- Exercise3/main.c
+++ Exercise3/main.c
@@ -4,11 +4,41 @@
 void selectionSortAlgorithm();
 void AscendingOrder();
 void multiplicationTable();
+void pickupMaxMin();
 int main() {
+    pickupMaxMin();
     multiplicationTable();
     selectionSortAlgorithm();
     AscendingOrder();
     return 0;
+}
+
+void pickupMaxMin()
+{
+    int a[3], i, max, step;
+    printf("Enter three integer numbers\n");
+    scanf("%d %d %d", &a[0], &a[1],&a[2]);
+    //ascending order those numbers
+    for(i=0; i<3; i++)
+    {
+        for(step=1+i;step < 3; step++)
+        {
+            if(a[i] > a[step])
+            {
+                max = a[i];
+                a[i] = a[step];
+                a[step] = max;
+                
+            }
+        }
+    }
+    printf("numbers printed as ascending order:\n");
+    for(i=0; i< 3; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\nThe smallest number is %d\n", a[0]);
+    printf("The  largest number is %d\n", a[2]);
 }
 void multiplicationTable()
 {
@@ -25,7 +55,7 @@ void multiplicationTable()
 void AscendingOrder()
 {
     int arr[5] = {20, 12, 10, 15, 2};
-    int min, max, i, step;
+    int min, i, step;
     for (i = 0; i < 5; i++) {
         for (step = i + 1; step < 5; step++) {
             if (arr[i] > arr[step]) {
@@ -44,7 +74,7 @@ void AscendingOrder()
 void selectionSortAlgorithm()
 {
     int arr[5] = {20, 12, 10, 15, 2};
-    int min, max, i, step;
+    int max, i, step;
 
     for (i = 0; i < 5; i++) {
         for (step = i + 1; step < 5; step++) {
