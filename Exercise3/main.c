d15ed1e1849d3d75f361fc8f4dece4f2db77fbe1
Qianfinland <qianfinland@gmail.com>
2015-08-01 20:43:20 +0300

    refactor code on select sort algorithm into methods

--- Exercise3/main.c
+++ Exercise3/main.c
@@ -1,46 +1,51 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+void selectionSortAlgorithm();
+void AscendingOrder();
 int main() {
-    int arr[5] = {20, 12, 10, 15, 2};
-    int min,max, i,step;
-    
-    for(i = 0; i < 5; i++)
-    {
-       for(step=i+1; step< 5; step++)
-       {
-           if(arr[i]<arr[step])
-           {
-               max = arr[step];
-               arr[step]=arr[i];
-               arr[i] = max;
-               
-           }
-       }
-    }
-    printf("Descending order:\n");
-    for(i=0; i <5; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    /*for(i = 0; i < 5; i++)
-    {
-       for(step=i+1; step< 5; step++)
-       {
-           if(arr[i]>arr[step])
-           {
-               min = arr[i];
-               arr[i] = arr[step];
-               arr[step] = min;
-           }
-       }
-    }
-    printf("Ascending order:\n");
-    for(i=0; i <5; i++)
-    {
-        printf("%d ", arr[i]);
-    }*/
+    selectionSortAlgorithm();
+    AscendingOrder();
     return 0;
 }
 
+void AscendingOrder()
+{
+    int arr[5] = {20, 12, 10, 15, 2};
+    int min, max, i, step;
+    for (i = 0; i < 5; i++) {
+        for (step = i + 1; step < 5; step++) {
+            if (arr[i] > arr[step]) {
+                min = arr[i];
+                arr[i] = arr[step];
+                arr[step] = min;
+            }
+        }
+    }
+    printf("Ascending order:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+void selectionSortAlgorithm()
+{
+    int arr[5] = {20, 12, 10, 15, 2};
+    int min, max, i, step;
+
+    for (i = 0; i < 5; i++) {
+        for (step = i + 1; step < 5; step++) {
+            if (arr[i] < arr[step]) {
+                max = arr[step];
+                arr[step] = arr[i];
+                arr[i] = max;
+
+            }
+        }
+    }
+    printf("Descending order:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
